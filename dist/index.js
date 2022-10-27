da9eaf8ba7ace58b9372ca2ea3d71768aa684ca4
Eduardo <eduardo.rocha2502@gmail.com>
2022-10-27 20:34:43 -0300

    pasta dist removida

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const Matemática = require('./Matemática');
-let n1 = 10;
-let n2 = 2;
-console.log(`Soma ${Matemática.somar(n1, n2)}`);
-console.log(`Subtração ${Matemática.subtrair(n1, n2)}`);
-console.log(`Multiplicar ${Matemática.multiplicar(n1, n2)}`);
