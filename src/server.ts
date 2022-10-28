57fb5b3654eb4ecebe63911e07837013fb220006
Eduardo <eduardo.rocha2502@gmail.com>
2022-10-28 12:00:18 -0300

    mustache adicionado

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
 import express, { Request, Response} from 'express';
+import path from 'path';
+import mustache from 'mustache-express';
 import mainRoutes from './routes/index';
 
 const server = express();
 
+//Template engines:
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
 server.use(mainRoutes);
+
+server.use((req: Request, res: Response)=>{
+    res.status(404).send('Página não encontrada');
+})
 
 server.listen(80);
